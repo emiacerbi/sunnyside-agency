9cf94a527a8d6e73722bc7ab4c2ca62e32986247
Emiliano Acerbi <eacerbip@gmail.com>
2022-02-25 09:02:55 -0300

    hiding arrow on mobile

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const mobileMenu = document.querySelector('.header__nav__menu')
 const hamburguer = document.querySelector('.fa.fa-bars')
 const triangle = document.querySelector('.triangle')
-
 const arrow = document.querySelector('.fa-solid.fa-arrow-down-long')
 
 let isShown = false
 
 hamburguer.addEventListener('click', () => {
-    if (!isShown) {
+    if (!isShown) 
+    {
         mobileMenu.style.opacity = '1'
         triangle.style.opacity = '1'
         isShown = true;
-    } else if (isShown)
+    } 
+    else if (isShown)
     {
         mobileMenu.style.opacity = '0'
         triangle.style.opacity = '0'
         isShown = false;
     }
 })
-
 
 arrow.addEventListener('click', () => {
     window.scrollBy(0,1000);
